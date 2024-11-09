783f6ccc4e33170134c2f34e6c02c05c0f696271
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2024-11-08 18:29:05 -0600

    Controllers

---
+++ CasoPractico1_G2/Controllers/CursoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasoPractico1_G2.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
