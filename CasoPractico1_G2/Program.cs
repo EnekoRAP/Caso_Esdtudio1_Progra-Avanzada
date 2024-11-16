2221f6bf43b534ae324acaf34d0938cb948a03a5
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-16 14:01:42 -0600

    Config pattern

--- CasoPractico1_G2/Program.cs
+++ CasoPractico1_G2/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,6 +20,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+name: "default",
+pattern: "{ controller = Home}/{ action = Index}/{ id ?}");
+});
 
 app.UseAuthorization();
 
