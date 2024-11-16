1843f313c9c6f0e5fc1912748fc1aeedeb0c9a9b
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-16 14:10:40 -0600

    Model y cshtml de Curso

---
+++ CasoPractico1_G2/Views/Home/Curso.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CasoPractico1_G2.Views.Home
+{
+    public class CursoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
