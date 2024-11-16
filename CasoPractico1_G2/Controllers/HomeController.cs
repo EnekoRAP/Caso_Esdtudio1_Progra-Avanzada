c8ca7b95d44d7c49a347591c20210f17c9d59123
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-16 13:55:18 -0600

    Nueva config para curso

--- CasoPractico1_G2/Controllers/HomeController.cs
+++ CasoPractico1_G2/Controllers/HomeController.cs
@@ -1,32 +1,21 @@
 using CasoPractico1_G2.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace CasoPractico1_G2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public IActionResult Curso()
         {
-            _logger = logger;
-        }
+            var cursos = new List<CursoModel>
+            {
+                new CursoModel {Cod_Curso = 203, Nombre = "Matemáticas Discretas", Descripcion = "Introducción a la Matemática Discreta"},
+                new CursoModel {Cod_Curso = 204, Nombre = "Fundamentos de Enrutamiento", Descripcion = "Introducción a métodos de Enrutamiento"},
+                new CursoModel {Cod_Curso = 205, Nombre = "Programación Avanzada", Descripcion = "Principios de la programación avanzada"}
+            };
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(cursos);
         }
     }
 }
