5b99a09e6345d8f2c9849999fa7639c07105789d
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-17 17:22:08 -0600

    Controller y Model de Curso

---
+++ CasoPractico1_G2/Controllers/CursoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CasoPractico1_G2.Models;
+using System.Collections.Generic;
+
+namespace CasoPractico1_G2.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Curso()
+        {
+            var cursos = new List<CursoModel>
+            {
+                new CursoModel { Cod_Curso = 1, Nombre = "Matemáticas", Descripcion = "Curso básico de álgebra y geometría" },
+                new CursoModel { Cod_Curso = 2, Nombre = "Ciencias", Descripcion = "Curso de física y química básica" },
+                new CursoModel { Cod_Curso = 3, Nombre = "Historia", Descripcion = "Curso de historia mundial" }
+            };
+
+            return View("Curso", cursos);
+        }
+    }
+}
