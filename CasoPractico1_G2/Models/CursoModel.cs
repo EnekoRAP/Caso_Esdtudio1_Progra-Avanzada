7eb3b370460ff4a47f1d512dedc70cf1f6291df7
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-17 17:15:31 -0600

    Deleting crashed project

--- CasoPractico1_G2/Models/CursoModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CasoPractico1_G2.Models
-{
-    public class CursoModel
-    {
-        public int Cod_Curso { get; set; }
-        public string Nombre { get; set; }
-        public string Descripcion { get; set; }
-    }
-}
--- CasoPractico1_G2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CasoPractico1_G2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
