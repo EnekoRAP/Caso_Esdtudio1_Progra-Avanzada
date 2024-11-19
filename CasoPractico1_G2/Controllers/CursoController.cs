9d4b2f575d4bfcfbdbc120c322547eb8bdab91ac
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-19 14:36:27 -0600

    CRUD de Curso

--- CasoPractico1_G2/Controllers/CursoController.cs
+++ CasoPractico1_G2/Controllers/CursoController.cs
@@ -1,22 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using CasoPractico1_G2.Models;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace CasoPractico1_G2.Controllers
 {
     public class CursoController : Controller
     {
+        private static List<CursoModel> cursos = new List<CursoModel>
+        {
+            new CursoModel { Cod_Curso = 1, Nombre = "Matemáticas", Descripcion = "Curso básico de álgebra y geometría" },
+            new CursoModel { Cod_Curso = 2, Nombre = "Ciencias", Descripcion = "Curso de física y química básica" },
+            new CursoModel { Cod_Curso = 3, Nombre = "Historia", Descripcion = "Curso de historia mundial" }
+        };
+
         public IActionResult Curso()
         {
-            var cursos = new List<CursoModel>
-            {
-                new CursoModel { Cod_Curso = 1, Nombre = "Matemáticas", Descripcion = "Curso básico de álgebra y geometría" },
-                new CursoModel { Cod_Curso = 2, Nombre = "Ciencias", Descripcion = "Curso de física y química básica" },
-                new CursoModel { Cod_Curso = 3, Nombre = "Historia", Descripcion = "Curso de historia mundial" }
-            };
-
-
             return View("~/Views/Home/Curso.cshtml", cursos);
+        }
+
+        [HttpPost]
+        public IActionResult AgregarCurso(CursoModel nuevoCurso)
+        {
+            nuevoCurso.Cod_Curso = cursos.Max(c => c.Cod_Curso) + 1;
+            cursos.Add(nuevoCurso);
+            return RedirectToAction("Curso");
+        }
+
+        public IActionResult ModificarCurso(CursoModel cursoActualizado)
+        {
+            var curso = cursos.FirstOrDefault(c => c.Cod_Curso == cursoActualizado.Cod_Curso);
+            if (curso != null)
+            {
+                curso.Nombre = cursoActualizado.Nombre;
+                curso.Descripcion = cursoActualizado.Descripcion;
+            }
+            return RedirectToAction("Curso");
         }
     }
 }
