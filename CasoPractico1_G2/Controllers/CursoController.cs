3ee55712b60f1fa777f79c03cb09733a661af1fd
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-11-19 14:40:52 -0600

    Update CursoController.cs

--- CasoPractico1_G2/Controllers/CursoController.cs
+++ CasoPractico1_G2/Controllers/CursoController.cs
@@ -25,7 +25,7 @@ namespace CasoPractico1_G2.Controllers
         {
             nuevoCurso.Cod_Curso = cursos.Max(c => c.Cod_Curso) + 1;
             cursos.Add(nuevoCurso);
-            return RedirectToAction("~/Views/Home/Curso.cshtml");
+            return RedirectToAction("~/Views/Home/Curso.cshtml/Eliminar");
         }
 
         public IActionResult ModificarCurso(CursoModel cursoActualizado)
@@ -36,7 +36,7 @@ namespace CasoPractico1_G2.Controllers
                 curso.Nombre = cursoActualizado.Nombre;
                 curso.Descripcion = cursoActualizado.Descripcion;
             }
-            return RedirectToAction("~/Views/Home/Curso.cshtml");
+            return RedirectToAction("~/Views/Home/Curso.cshtml/AgregarCurso");
         }
     }
 }
